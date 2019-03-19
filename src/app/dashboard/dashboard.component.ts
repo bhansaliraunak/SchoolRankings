a4bb9440aba80f4156d98f8a8189a6baa81d6f41
Alexander Torrijos <artorrijos@gmail.com>
2019-03-19 20:49:06 +0800

    Fixed display of schools

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,6 @@ import { map } from 'rxjs/operators';
 
 import { PagerService } from '../services/pager.service';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,56 +12,53 @@ import { PagerService } from '../services/pager.service';
 })
 export class DashboardComponent implements OnInit {
 
+  constructor(private http: HttpClient, private pagerService: PagerService) {}
 
-  constructor(private http: HttpClient, private pagerService: PagerService) { }
+  // array of all items to be paged
+  private allItems = new Array();
+  data = [];
 
-    // array of all items to be paged
-    private allItems = new Array();
+  // pager object
+  pager: any = {};
 
-    data =[];
+  // paged items
+  pagedItems: any[];
 
-    // pager object
-    pager: any = {};
+  page = 1;
 
-    // paged items
-    pagedItems: any[];
-
-    page=1;
-
-    ngOnInit() {
-        this.getData(this.page);
-    }
-
-    getData(page: number){
-      this.http.get('http://localhost:8000/api/v1/schools?page= '+this.page)
-            .pipe(map((response: any) => response))
-            .subscribe((data: any) => {
-
-              console.log('Data: ',data);
-                // set items to json response
-                if(data.length>0){
-                  for(let i=0; i<data.length; i++){
-                    this.allItems.push(data[i]);
-                } 
-              
-
-                    this.setPage(1);
-                    this.getData(this.page++);
-                }
-              
-                 
-            
-                               
-            });
-    }
-
-    setPage(page: number) {
-        // get pager object from service
-        console.log(this.allItems);
-        this.pager = this.pagerService.getPager(this.allItems.length, page);
-      
-        // get current page of items
-        this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex+1);
-    }
-
+  ngOnInit() {
+    this.getData(this.page);
   }
+
+  getData(page: number) {
+    this.http
+      .get('http://localhost:8000/api/v1/schools?page= ' + this.page)
+      .pipe(map((response: any) => response))
+      .subscribe((data: any) => {
+        console.log('Data: ', data);
+
+        // set items to json response
+        const len = Object.keys(data).length;
+        if (len > 0) {
+          // tslint:disable-next-line:prefer-for-of
+          for (let i = 0; i < len; i++) {
+            this.allItems.push(data[i]);
+          }
+          this.setPage(1);
+          this.getData(this.page++);
+        }
+      });
+  }
+
+  setPage(page: number) {
+    // get pager object from service
+    console.log(this.allItems);
+    this.pager = this.pagerService.getPager(this.allItems.length, page);
+
+    // get current page of items
+    this.pagedItems = this.allItems.slice(
+      this.pager.startIndex,
+      this.pager.endIndex + 1
+    );
+  }
+}
